3d9300a6d31c483beba80875a21d746cf3377fe0
slymn_LAB2 <ezfesoftware@gmail.com>
2024-12-18 10:48:42 +0300

    Proje dosyası ekle.

---
+++ bgt1_gorsel_18Ara-5/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bgt1_gorsel_18Ara_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int satir= Convert.ToInt32(textBox1.Text);
+            for (int i = 1; i <= satir; i++)
+            {
+                string yildiz = "";
+                string bosluk = "";
+                for (int a = 0; a < satir-i; a++)
+                {
+                    bosluk += " ";
+                }
+                for (int j=1;j<=i;j++)
+                {
+                    yildiz += "* ";
+                }
+                listBox1.Items.Add(bosluk+yildiz);
+
+                /*
+                
+                |    *
+                |   * *
+                |  * * *
+                | * * * * 
+
+                */
+            }
+        }
+    }
+}
---
+++ bgt1_gorsel_18Ara-5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("bgt1_gorsel_18Ara-5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bgt1_gorsel_18Ara-5")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("09902e2b-d11a-4a0e-888b-7c5fbfa59fe1")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
